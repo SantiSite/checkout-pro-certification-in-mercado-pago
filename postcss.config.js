0b0e867ed310fd91b77433389383eeee872c324d
Santiago Restrepo <santiagorv246@gmail.com>
2022-09-21 03:28:11 -0500

    fix 2

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,6 @@ module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
-        ...(process.env.NODE_ENV === 'production'
-            ? require('cssnano')({ preset: 'default' })
-            : {})
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
     }
 }
