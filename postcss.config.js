bbeea410c21bf31807d4cc0fca72b9f5d8b0e153
Santiago Restrepo <santiagorv246@gmail.com>
2022-09-21 03:16:53 -0500

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
-        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+        ...(process.env.NODE_ENV === 'production'
+            ? require('cssnano')({ preset: 'default' })
+            : {})
     }
 }
